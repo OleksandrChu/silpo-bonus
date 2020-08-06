69fb4be3f7824c26b7f806c3911686dcf8a43766
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-08-06 15:19:31 +0300

    [Refactored] Use Linq to calc totalCost and totalPoints

--- SilpoBonusCore/checkout/Check.cs
+++ SilpoBonusCore/checkout/Check.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace SilpoBonusCore.checkout
@@ -8,31 +8,18 @@ namespace SilpoBonusCore.checkout
         private List<Product> products = new List<Product>();
         private int points = 0;
 
-        public int GetTotalCost()
-        {
-            int totalCost = 0;
-            products.ForEach(product => totalCost+=product.price);
-            return totalCost;
-        }
-
-        internal void AddProduct(Product product) =>products.Add(product);
+        public int GetTotalCost() => products.Sum(product => product.price);
+        internal void AddProduct(Product product) => products.Add(product);
 
         public int GetTotalPoints() => GetTotalCost() + points;
 
-        public void AddPoints(int points) => this.points += points;
+        internal void AddPoints(int points) => this.points += points;
 
         internal int GetCostByCategory(Category category)
         {
-            int cost = 0;
-            foreach (Product product in products)
-            {
-                if (product.category == category)
-                {
-                    cost += product.price;
-                }
-            }
-
-            return cost;
+            return products
+            .Where(product => product.category.Equals(category))
+            .Sum(product => product.price);
         }
     }
 }
